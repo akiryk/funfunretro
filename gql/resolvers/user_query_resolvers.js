c8be4ea4999e768e3d15f9afc5ee4cd07f0dc406
Adam Kiryk <akiryk@wayfair.com>
2020-04-12 14:20:55 -0400

    Fix user returning uid

--- gql/resolvers/user_query_resolvers.js
+++ gql/resolvers/user_query_resolvers.js
@@ -15,6 +15,7 @@ const errorMsg = [
       success: false,
     },
     id: '',
+    uid: '',
   },
 ];
 
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /**
  * Index
  *
- * https://us-east1-funfunretro.cloudfunctions.net/api/boards
  */
 
 const functions = require('firebase-functions');
@@ -15,18 +14,7 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
-// Endpoint
-exports.api = functions.region('us-east1').https.onRequest(app);
+// Graphql endpoing: https://us-east1-funfunretro.cloudfunctions.net/api/
+exports.graphql = functions.region('us-east1').https.onRequest(app);
 
-// Graphql
-server.applyMiddleware({ app, path: '/graphql', cors: true });
-
-// Rest
-// const { signup, login } = require('./handlers/users');
-// const { getBoards } = require('./handlers/boards');
-// const { getColumns } = require('./handlers/columns');
-
-// app.post('/signup', signup);
-// app.post('/login', login);
-// app.get('/boards', FbAuth, getBoards);
-// app.get('/columns', getColumns);
+server.applyMiddleware({ app, path: '/', cors: true });
