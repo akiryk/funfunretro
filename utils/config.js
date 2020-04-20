e14ee3001525afe8818ab2764a62db828fbc87dd
Adam Kiryk <akiryk@wayfair.com>
2020-04-20 13:14:51 -0400

    Set better git ignore

---
+++ utils/config.js
@@ -0,0 +1,12 @@
+// config
+
+module.exports = {
+  apiKey: 'AIzaSyD_oGtyzOu_e-Pgm8s9yZjDYetW5ab5BP4',
+  authDomain: 'funfunretro.firebaseapp.com',
+  databaseURL: 'https://funfunretro.firebaseio.com',
+  projectId: 'funfunretro',
+  storageBucket: 'funfunretro.appspot.com',
+  messagingSenderId: '944978282083',
+  appId: '1:944978282083:web:700b549d11145448b15a56',
+  measurementId: 'G-TJ96WJFW5G',
+};
